fd6ce0f1da48fda37fc5b8658a7e78a3c853a081
Rafal Kozlowski <44146126+wakiter@users.noreply.github.com>
2022-04-23 14:05:04 +0200

    initial commit with scaffolded project

---
+++ src/test/java/kozlowski/rafal/publicLibrary/PublicLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package kozlowski.rafal.publicLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
